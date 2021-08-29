5807acd3462203f4db06a304e6db8b3cf63edba0
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-08-28 20:03:34 -0500

    m

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-
 const navSlide = () => {
     const burger = document.querySelector(".hamburger");
     const nav = document.querySelector(".nav-links");
@@ -21,39 +20,22 @@ const navSlide = () => {
     });
 };
 navSlide();
-// Video
-// var vid = document.querySelector("video");
-// vid.playbackRate = 0.6;
-//Loader JS
 
-// document.onreadystatechange = function() {
-//     if (document.readyState !== "complete") {
-//         document.querySelector("#nav").style.display = "none";
-//         document.querySelector("#main").style.display = "none";
-//         document.querySelector("#loader").style.visibility = "block";
-//     } else {
-//         document.querySelector("#loader").style.display = "none";
-//         document.querySelector("#main").style.display = "block";
-//         document.querySelector("#nav").style.display = "block"; 
-//     }
-// };
-// JS for reverse navSlide for when user clicks on nav link item to redirect to section on same page and automatically closes the 
-// nav for them
 
 const reverseSlide = () => {
-  const burger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+    const burger = document.querySelector(".hamburger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
     //Toggle Nav
     nav.classList.toggle("is-active");
     //Animate Links
 
     navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade .1s ease-in forwards`;
-      }
+        if (link.style.animation) {
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinkFade .1s ease-in forwards`;
+        }
     });
     //burger animation
     burger.classList.toggle("is-active");
@@ -63,36 +45,19 @@ const reverseSlide = () => {
 
 const navLink2 = document.querySelector('.nav-links');
 navLink2.addEventListener("click", reverseSlide);;
-// Popup 
 
-const popup = document.querySelector(".popup");
-const closeBtn = document.querySelector(".close-btn");
-const enablePopup = () => {
-    popup.style.display = 'block';
-}
-// closeBtn.addEventListener("click", () => {
-//     popup.style.display = 'none';
-// })
+// Landing Swiper.js Init
 
-window.onload = () => (
-    enablePopup()
-)
-
-
-// Set a variable for our button element.
-const scrollToTopButton = document.getElementById("js-top");
-
-// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
-const scrollFunc = () => {
-    // Get the current scroll value
-    let y = window.scrollY;
-
-    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
-    if (y > 0) {
-        scrollToTopButton.className = "top-link show";
-    } else {
-        scrollToTopButton.className = "top-link hide";
-    }
-};
-
-window.addEventListener("scroll", scrollFunc);
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
